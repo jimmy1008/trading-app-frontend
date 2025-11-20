398fd8411b30f21088bee0aa3cc6ea9a1baa7dab
jimmy1008 <wu992901212@gmail.com>
2025-11-21 07:16:41 +0800

    Fix static route mapping

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "login.html"));
 });
 
-const PORT = process.env.PORT || 8080;
+app.get("/:page", (req, res) => {
+  const filePath = path.join(__dirname, req.params.page);
+  res.sendFile(filePath, err => {
+    if (err) res.status(404).send("Not found");
+  });
+});
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Frontend running on port", PORT);
 });
-
